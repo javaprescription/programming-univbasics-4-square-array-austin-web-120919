516f8085ca8b8ec6a4ff3ba9aae02faf7919d970
Austin Scott <austinscott4@gmail.com>
2019-12-04 20:06:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   counter = 0
-  while array[counter] do
-    puts array[counter**2]
+  while counter < array.length do
+    array2 = []
+    array2.push(array[counter**2])
     counter += 1
   end
 end
